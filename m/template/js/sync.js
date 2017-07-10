cef30b5a951ea37dbc61b958c5e89be227398929
Gao Yi <gaoyicoder@163.com>
2017-07-10 16:38:26 +0800

    sync

---
+++ m/template/js/sync.js
@@ -0,0 +1,30 @@
+function keepOnLine(){
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+
+    }
+}
+
+function showPosition(position) {
+    var lat = position.coords.latitude;
+    var lng = position.coords.longitude;
+
+    $.ajax({
+        url:'sync.php',
+        type:"POST",
+        dataType:"json",
+        data:{
+            lat:lat,
+            lng:lng
+        },
+        success:function(data){
+
+        }
+
+
+    });
+}
+
+$(function() {
+    setInterval("keepOnLine()",1000);
+});

28f224f46d18c87068e3276e7bf8569083289ed3
Gao Yi <gaoyicoder@163.com>
2017-07-12 22:38:29 +0800

    a

---
+++ m/include/inc_sync_map.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gaoyi
+ * Date: 7/12/17
+ * Time: 4:35 PM
+ */
+
+$catid = $_REQUEST['catid'] ? $_REQUEST['catid'] : 0;
+if($catid) {
+
+    $row = $db->getAll("SELECT cs.* FROM `{$db_mymps}coords_sync` AS cs
+                          INNER JOIN `{$db_mymps}information` AS i
+                          ON cs.userid = i.userid
+                          WHERE i.catid= $catid");
+    include mymps_tpl('sync_map');
+} else {
+    header("Location: index.php");
+}

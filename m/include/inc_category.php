ac0b3d6c3eb3e9c0f8d7e328112a4e36a2766dcf
Gao Yi <gaoyicoder@163.com>
2017-07-19 17:55:45 +0800

    t

--- m/include/inc_category.php
+++ m/include/inc_category.php
@@ -137,10 +137,10 @@ foreach($infolist as $k => $row){
     if($distance) {
         if (in_array($arr['id'], $id_online)) {
             $arr['is_online'] = 1;
-            $arr['info_distance'] = round(calculate_distance($lat, $lng, $row['latitude'], $row['longitude']), 2);
-        } else {
             $row_cs = $db->getOne("SELECT * FROM `{$db_mymps}coords_sync` AS cs WHERE userid ='".$row['userid']."'");
-            $arr['info_distance'] = round(calculate_distance($lat, $lng, $row_cs['lat'], $row_cs['lng']), 2);
+            $arr['info_distance'] = round(calculate_distance($lat, $lng, $row_cs['latitude'], $row_cs['longitude']), 2);
+        } else {
+            $arr['info_distance'] = round(calculate_distance($lat, $lng, $row['lat'], $row['lng']), 2);
         }
     }
 

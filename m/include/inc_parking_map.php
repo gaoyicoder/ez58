1ab092987360ef492e73c27e0ca4c5aade95b7ea
Gao Yi <gaoyicoder@163.com>
2017-11-09 15:27:18 +0800

    asd

--- m/include/inc_parking_map.php
+++ m/include/inc_parking_map.php
@@ -9,8 +9,7 @@
 $catid = $_REQUEST['catid'] ? $_REQUEST['catid'] : 0;
 if($catid) {
 
-    $row = $db->getAll("SELECT i.* as info_id, i.title  FROM `{$db_mymps}information` AS i
-                          WHERE i.catid= $catid");
+    $row = $db->getAll("SELECT i.* as info_id, i.title  FROM `{$db_mymps}information` AS i WHERE i.catid= $catid");
     include mymps_tpl('parking_map');
 } else {
     header("Location: index.php");

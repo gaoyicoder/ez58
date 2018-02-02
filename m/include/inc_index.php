84eb9e28ae95f842d2f4de88cab29cee2a133176
Gao Yi <gaoyicoder@163.com>
2018-02-02 13:39:56 +0800

    asd

--- m/include/inc_index.php
+++ m/include/inc_index.php
@@ -1,6 +1,13 @@
 <?php
 CURSCRIPT != 'wap' && exit('FORBIDDEN');
-
+$has_unread = 0;
+if($s_uid) {
+    $row_info = $db -> getRow("SELECT * FROM `{$db_mymps}information` WHERE userid = '$s_uid' AND read_status = 0 AND book_uid != '' ORDER BY begintime DESC");
+    if ($row_info) {
+        $has_unread = $row_info['id'];
+        $db->query("UPDATE `{$db_mymps}information` SET read_status=1 WHERE userid = '{$s_uid}'");
+    }
+}
 include mymps_tpl('index');
 
 ?>
--- m/include/inc_parking_book_status.php
+++ m/include/inc_parking_book_status.php
@@ -11,6 +11,7 @@ $row = $db -> getRow("SELECT * FROM `{$db_mymps}information` WHERE id = '$id'");
 
 if($row) {
     if($row['book_uid']!="") {
+        $db->query("UPDATE `{$db_mymps}information` SET read_status=1 WHERE id = '{$id}'");
         $row['book_mobile'] = $db -> getOne("SELECT mobile FROM `{$db_mymps}member` WHERE userid = '".$row['book_uid']."'");
         $result = true;
     }

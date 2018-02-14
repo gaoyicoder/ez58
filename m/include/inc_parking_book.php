5923bd7a1a5cffe6cc0b27dab945402f0073c0b2
Gao Yi <gaoyicoder@163.com>
2018-02-14 23:37:27 +0800

    asd

--- m/include/inc_parking_book.php
+++ m/include/inc_parking_book.php
@@ -7,16 +7,26 @@
  */
 
 $result = false;
+$status = 0;
 if ($s_uid) {
     $info_id = $_REQUEST['id'] ? $_REQUEST['id'] : '';
     if($info_id) {
         $row = $db->getRow("SELECT * FROM `{$db_mymps}information` WHERE id = '{$info_id}'");
         if($row) {
-            if($row['book_uid'] == '' ) {
-                $result = true;
-                $db->query("UPDATE `{$db_mymps}information` SET book_uid='{$s_uid}',book_status=0 WHERE id = '{$info_id}'");
+            $row_user = $db->getRow("SELECT * FROM `{$db_mymps}member` WHERE userid = '{$s_uid}'");
+            if ($row_user['money_own'] < $row['content']) {
+                $result = false;
+                $status = 1;
+            } else {
+                if($row['book_uid'] == '' ) {
+                    $result = true;
+                    $db->query("UPDATE `{$db_mymps}information` SET book_uid='{$s_uid}',book_status=0 WHERE id = '{$info_id}'");
+                } else {
+                    $result = false;
+                    $status = 2;
+                }
             }
         }
     }
 }
-echo json_encode(array('result'=>$result));
+echo json_encode(array('result'=>$result,'status' => $status));
